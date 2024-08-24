6cf599fc0f605bd5dc68674eb9d657a0e80da8f6
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2024-08-25 02:20:47 +0630

    new

---
+++ main.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+from pathlib import Path
+import env
+
+client = OpenAI(api_key=env.OPENAI_API_KEY)
+response = client.files.create(
+  file=Path('food_suggestion_prompt.jsonl'),
+  purpose='fine-tune'
+)
+
+fine_tune_response = client.fine_tuning.jobs.create(
+  training_file=response.id,  # Use the uploaded file's ID
+  model="gpt-4o-mini-2024-07-18"       # Specify the model you want to fine-tune
+)
+
+print("Fine-tuning job started with ID:", fine_tune_response.id)
